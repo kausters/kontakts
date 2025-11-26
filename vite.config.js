1c98062aef2e7e991173229174ff0d099a40a707
Kristaps Austers <kristaps@kontakts.lv>
2025-11-27 01:34:45 +0200

    Migrate to NPM project structure, build with Vite

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: 'src',
+  base: '/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(import.meta.dirname, 'src/index.html'),
+        darbi: resolve(import.meta.dirname, 'src/darbi/index.html')
+      }
+    }
+  },
+  server: {
+    strictPort: false,
+  }
+})
